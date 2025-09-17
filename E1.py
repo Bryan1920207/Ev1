ea7875bb65e835f2f04484dd10063f23c60afeb3
Bryan Eduardo Niño Contreras <bryan.ninocn@uanl.edu.mx>
2025-09-17 16:58:40 -0600

    Inicio1

--- E1.py
+++ E1.py
@@ -1,8 +1,18 @@
-#import pandas as pd
+import pandas as pd
+import datetime
 
-x=int(input("Sala a registrar: "))
+diccionario_evento={"Nombre":[],
+             "Cupo":[],
+             "Fecha":[]}
+diccionario_cliente={"Nombre":[],
+             "Apellido":[]}
+diccionario_sala={"Nombre":[],
+             "Cupo":[]}
+diccionario_datos=pd.DataFrame(diccionario_evento)
+diccionario_datos=pd.DataFrame(diccionario_cliente)
+diccionario_datos=pd.DataFrame(diccionario_sala)
 while True:
-    if x==0:
+    if diccionario_sala==0:
         pass
     else:
         break
