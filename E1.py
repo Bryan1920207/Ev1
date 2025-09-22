97b0971eb53551b91536854db9b94013facc9794
Bryan Eduardo Niño Contreras <bryan.ninocn@uanl.edu.mx>
2025-09-22 17:07:57 -0600

    Avance

--- E1.py
+++ E1.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import datetime
 
-diccionario_evento={"Nombre":[],
-             "Cupo":[],
-             "Fecha":[]}
-diccionario_cliente={"Nombre":[],
-             "Apellido":[]}
-diccionario_sala={"Nombre":[],
-             "Cupo":[],
-             "Turno":[]}
+diccionario_evento={"Nombre":["Conferencia","Reunion",""],
+             "Cupo":[50,100,275],
+             "Fecha":["10/01/2020","//","//"]}
+diccionario_cliente={"Nombre":["","",""],
+             "Apellido":["","",""]}
+diccionario_sala={"Nombre":["","",""],
+             "Cupo":[50,100,275],
+             "Turno":["M","N","V"]}
+if diccionario_sala["Turno"] not in "MNVmnv":
+    print("Turno invalido")
+    
+datetime.datetime.strptime(diccionario_evento["Fecha"], "%d/%m/%Y").date()
+if pd.DataFrame(diccionario_evento["Cupo"]<=diccionario_sala["Cupo"]):
+    pass
 diccionario_datos=pd.DataFrame(diccionario_evento)
 diccionario_datos=pd.DataFrame(diccionario_cliente)
 diccionario_datos=pd.DataFrame(diccionario_sala)
+print("1.Registrar cliente\n2.Registrar sala\n3.Editar reservacion\n4.Salir")
+opcion=int(input("Que opcion desea hacer? "))
 while True:
-    if diccionario_sala==0:
+    if opcion==1:
         pass
-    else:
+    elif opcion==2:
+        pass
+    elif opcion==3:
         break
